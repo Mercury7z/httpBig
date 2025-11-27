c5002d0606c79915dbe00074bf26cc07a4288bfa
KimBogdan <bogger.kim@gmail.com>
2025-11-27 21:09:09 +0600

    #2 не класс регистрации

---
+++ src/util/Registration.java
@@ -0,0 +1,21 @@
+package util;
+
+
+import ServerLogic.DataModel;
+import model.Client;
+
+public class Registration {
+    public static boolean registration(DataModel DataModel,String login,String pass) {
+        if (!isAlreadyRegistered(DataModel,login,pass)) {
+           DataModel.getClients().put(login + pass,Client.getClient(login,pass));
+           return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private static boolean isAlreadyRegistered(DataModel DataModel, String login, String pass) {
+       return DataModel.getClients().containsKey(login + pass);
+    }
+}
