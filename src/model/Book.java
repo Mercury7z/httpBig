8746c1bcab76a41cc35ed2e3408d76483c0b2a7c
KimBogdan <bogger.kim@gmail.com>
2025-12-05 14:34:01 +0600

    #2 добавил переменную пути в класс книги

--- src/model/Book.java
+++ src/model/Book.java
@@ -4,11 +4,13 @@ public  class Book {
     private String name;
     private String author;
     private String key;
+    private String path;
 
     private Book(String name,String author,String key){
         this.name = name;
         this.author = author;
         this.key = key;
+        this.path = "/data/img/1.jpg";
     }
 
     public static Book getBook(String name,String author,String key) {
